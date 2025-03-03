1124cbda9b7f563181f2272e38cd8bc9cee1b8ae
rajeswarisennamsetty <rajeswarisennamsetty@gmail.com>
2025-03-03 22:11:51 +0530

    first time pushing my project

---
+++ index.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const vendorRoutes=require('./routes/vendorRoutes');
+const bodyParser=require('body-parser');
+const firmRoutes=require('./routes/firmRoutes');
+const productRoutes=require('./routes/productRoutes');
+const cors=require('cors');
+const path=require('path');
+const app=express();
+const PORT=4000;
+dotenv.config();
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("Mongodb connected"))
+    .catch((error)=>console.log(error))
+app.use(bodyParser.json());
+app.use('/Vendor',vendorRoutes);
+app.use('/Firm',firmRoutes);
+app.use('/uploads', express.static('uploads'));
+app.use('/product',productRoutes);
+
+
+
+app.listen (PORT,()=>{
+    console.log("server started and running at ${PORT}");
+});
+app.use('/home',(req,res)=>{
+    res.send("<h1>Welcome to TastyGo");
+
+});
